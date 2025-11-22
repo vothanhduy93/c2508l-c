d3008f4532ecf3ed8bbbffaaf77a8505cbd3db3e
Võ Thanh Duy <hcmc.duyvo@gmail.com>
2025-11-22 10:36:04 +0700

    Add files via upload

---
+++ buoi1.c
@@ -0,0 +1,12 @@
+// comment code (ghi chu code)
+// day la khai bao thu vien stdio (standard input output: thu vien nhap xuat chuan cua C)
+#include "stdio.h"
+
+// Ham main la ham dac biet se duoc goi khi chuong trinh chay
+int main(){
+	// viet code o day
+	// printf (print formartted) dung de in ra man hinh du lieu co dinh dang
+	printf("Hello");
+	
+	return 0;
+}
