87362a6f6a9d0c4fc6248c1489dd8907d3836bd3
Võ Thanh Duy <hcmc.duyvo@gmail.com>
2025-11-28 21:45:24 +0700

    Add files via upload

---
+++ buoi-3.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+	
+	// Declare variable (khai bao bien)
+	// nghia la tao 1 bien ma khong gan gia tri
+	// syntax: kieu_du_lieu ten_bien;
+	
+//	int a;
+//	float b;
+//	double c;
+//	char d;
+	
+	// Initialize variable (khoi tao bien)
+	// nghia la tao 1 bien va gan gia tri cho bien do
+//	int a = 5;
+//	float b = 8.9;
+//	double c = 9.5555555;
+//	char d = 'A';
+	
+	// printf: la ham in du lieu ra man hinh 
+	// scanf: la nhap du lieu tu ban phim
+	
+	// Yeu cau: nguoi dung nhap so a, so b. Sau do in ra man hinh
+	// ket qua cua a - b
+	
+//	int a;
+//	int b;
+//	
+//	printf("Nhap so a:");
+//	scanf("%d", &a);
+//	
+//	printf("Nhap so b:");
+//	scanf("%d", &b);
+//	
+//	printf("a - b = %d", a - b);
+
+//	int a, b;
+//	
+//	printf("Nhap so a: ");
+//	scanf("%d", &a);
+//	printf("Nhap so b: ");
+//	scanf("%d", &b);
+//	printf("a / b = %.2f", (float)a / b);
+//	
+//	printf("\n");
+	
+//	printf("Gia tri cua bien a la: %d \n", a);
+//	printf("Dia chi cua bien a trong RAM la: %p", &a);
+	
+//	int a = 1, b = 2;
+//	
+//	printf("%d chia lay du cho %d la %d", a, b, a % b);
+	
+//	int a = 5;
+//	printf("Gia tri cua bien a la: %d \n", a);
+//	printf("Gia tri cua bien a sau thay doi la: %d \n", a++);
+//	printf("Gia tri cua bien a la: %d", a);
+	
+//	int a = 5;
+//	a = --a + 1 + a--;
+//	
+//	printf("Gia tri cua a la: %d", a);
+	
+	
+//	int a = 5;
+//	printf("a += 5 la: %d \n", a += 5);
+//	printf("a -= 5 la: %d \n", a -= 5);
+//	printf("a *= 5 la: %d \n", a *= 5);
+//	printf("a /= 5 la: %d \n", a /= 5);
+//	printf("a chia lay du cho 5 la: %d \n", a %= 5);
+	
+	int x = 5, y = 3;
+	printf("x > y: %d \n", x > y);
+	printf("x < y: %d \n", x < y);
+	printf("x = y: %d \n", x == y);
+	printf("x >= y: %d \n", x >= y);
+	printf("x <= y: %d \n", x <= y);
+	printf("x != y: %d \n", x != y);
+	
+	
+	return 0;
+}
